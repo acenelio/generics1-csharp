95577ae1e078aa4bcf2a369c550af250e36a17af
Nelio Alves <acenelio@gmail.com>
2018-05-12 06:35:59 -0300

    Initial implementation

---
+++ Course/PrintService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Course {
+    class PrintService<T> {
+
+        private T[] _values = new T[10];
+        private int _cont = 0;
+
+        public void AddValue(T value) {
+            if (_cont == 10) {
+                throw new InvalidOperationException("PrintService is full");
+            }
+            _values[_cont] = value;
+            _cont++;
+        }
+
+        public T First {
+            get { return _values[0]; }
+        }
+
+        public void Print() {
+            Console.Write("[");
+            for (int i = 0; i < _cont - 1; i++) {
+                Console.Write(_values[i] + ", ");
+            }
+            if (_cont > 0) {
+                Console.Write(_values[_cont - 1]);
+            }
+            Console.WriteLine("]");
+        }
+    }
+}
---
+++ Course/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Course {
+    class Program {
+        static void Main(string[] args) {
+
+            PrintService<int> printService = new PrintService<int>();
+
+            Console.Write("How many values? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++) {
+                int x = int.Parse(Console.ReadLine());
+                printService.AddValue(x);                
+            }
+
+            printService.Print();
+            Console.WriteLine("First: " + printService.First);
+        }
+    }
+}
