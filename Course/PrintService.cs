19120f56851ba179af8d5f2519f7a4ad5d6f9f56
Nelio Alves <acenelio@gmail.com>
2020-05-23 14:22:15 -0300

    Class

--- Course/PrintService.cs
+++ Course/PrintService.cs
@@ -4,27 +4,30 @@ namespace Course {
     class PrintService<T> {
 
         private T[] _values = new T[10];
-        private int _cont = 0;
+        private int _count = 0;
 
         public void AddValue(T value) {
-            if (_cont == 10) {
+            if (_count == 10) {
                 throw new InvalidOperationException("PrintService is full");
             }
-            _values[_cont] = value;
-            _cont++;
+            _values[_count] = value;
+            _count++;
         }
 
-        public T First {
-            get { return _values[0]; }
+        public T First() {
+            if (_count == 0) {
+                throw new InvalidOperationException("PrintService is empty");
+            }
+            return _values[0];
         }
 
         public void Print() {
             Console.Write("[");
-            for (int i = 0; i < _cont - 1; i++) {
+            for (int i = 0; i < _count - 1; i++) {
                 Console.Write(_values[i] + ", ");
             }
-            if (_cont > 0) {
-                Console.Write(_values[_cont - 1]);
+            if (_count > 0) {
+                Console.Write(_values[_count - 1]);
             }
             Console.WriteLine("]");
         }
--- Course/Program.cs
+++ Course/Program.cs
@@ -11,11 +11,11 @@ namespace Course {
 
             for (int i = 0; i < n; i++) {
                 int x = int.Parse(Console.ReadLine());
-                printService.AddValue(x);                
+                printService.AddValue(x);
             }
 
             printService.Print();
-            Console.WriteLine("First: " + printService.First);
+            Console.WriteLine("First: " + printService.First());
         }
     }
 }
